7d8572d5f042f00bd0fe407cee6637fa0e6ccc6d
paulo vitor <paulovitor.dev.adm@gmail.com>
2026-09-11 08:24:15 -0300

    tipos de dados number

---
+++ tipos de dados number.js
@@ -0,0 +1,7 @@
+let idade = 25; //Número inteiro
+let altura = 1.75; // Número de ponto flutuante
+let temperatura = -5; // Número negativo
+
+console.log ("idade", idade)
+ console.log ("altura", altura)
+ console.log ("temperatura", temperatura)
