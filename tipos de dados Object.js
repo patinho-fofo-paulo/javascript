74483812caa1b840ffd7e6c63589ea02af21e9e8
paulo vitor <paulovitor.dev.adm@gmail.com>
2026-09-11 09:56:08 -0300

    add.tipos de dados object

---
+++ tipos de dados Object.js
@@ -0,0 +1,9 @@
+let pessoa = {
+nome: "Paulo",
+idade: 15,
+casado: false,
+};
+//acessando propriedade de objeto
+console.log (pessoa.nome) // "Paulo"
+console.log (pessoa.idade) // 15
+console.log (pessoa.casado) // false
