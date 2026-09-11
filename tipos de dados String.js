d61bb48b6670c6479e8b497cbbf0c1e832d22ed0
paulo vitor <paulovitor.dev.adm@gmail.com>
2026-09-11 08:29:15 -0300

    tipos de dados string

---
+++ tipos de dados String.js
@@ -0,0 +1,7 @@
+let nome = "Ana";// string com aspas simples
+let sobrenome = "silveira"; // string vom aspas simples
+let saudacao = 'olá, $(nome)!'; //template literal com interpolação de variavel
+
+console.log ("meu nome é", nome)
+console.log ("meu sobrenome é", sobrenome)
+console.log (saudacao)
