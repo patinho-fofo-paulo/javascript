07d091e930c906d6a34c2004c6bd64d9bcdf89eb
paulo vitor <paulovitor.dev.adm@gmail.com>
2026-09-11 09:54:32 -0300

    add.tipos de dado array

--- tipos de dados array .js
+++ tipos de dados array .js
@@ -1,3 +1,7 @@
-let fruutas = [goiaba,laranja,banana];//array de string
+let frutas = [goiaba,laranja,banana];//array de string
 let numeros = [1, 2, 3, 4, 5];//array de numeros
 let misto = [true,"texto",67] //array com diferentes tipos de dados
+
+console.log("frutas", "goiaba", "laranja", "banana");
+console.log("numeros", 1, 2, 3, 4, 5);
+console.log("misto", true, "texto", 67);
