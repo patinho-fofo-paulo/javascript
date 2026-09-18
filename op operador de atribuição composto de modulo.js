e761e4c6b7f9f68edefaf9f8fcd4245fcdbce2b9
senacmatheus2026-cell <paulovitor.dev.adm@gmail.com>
2026-09-18 09:47:11 -0300

    add operador de modulo

---
+++ op operador de atribuição composto de modulo.js
@@ -0,0 +1,8 @@
+//11.atribuidor composto de modulo
+let totalDoces= 10;
+let criancas =3;
+
+//queremos saber quantos doces sobram apos dividir igualmente
+totalDoces %= criancas;
+
+console.log(totalDoces);//1 pois dividir 10 dividido por 3 por 1
